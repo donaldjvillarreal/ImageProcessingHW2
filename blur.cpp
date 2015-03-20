4941442bb075f98e39f5b8b8990accf09120f2c6
Donald Villarreal <donaldjvillarreal@gmail.com>
2015-03-20 12:48:08 -0400

    cleaned up the code for blur and sharpen.  Added comments to the codes.  They still don't match the xip output, but I have a suspician that he's using linear interpolation for the padding.

--- blur.cpp
+++ blur.cpp
@@ -1,6 +1,3 @@
-
-//IN PROGRESS!
-
 ///////
 // ================================================================
 // blur.cpp - Directional blur program
@@ -9,9 +6,7 @@
 //             Joseph Wagner
 // =====================================================================
 
-#include <vector>
 #include "IP.h"
-
 using namespace std;
 
 // function prototype
@@ -73,66 +68,60 @@ main(int argc, char** argv)
 void
 blur(imageP I1, int xsz, int ysz, imageP I2)
 {
-	int i, x, y, xpad, ypad, total, pad_itr, mysum, out_itr, w, h;
+	int i, w_itr, h_itr, pad, total, pad_itr, mysum, out_itr, w, h;
 	int xline[I1->width+xsz], yline[I1->height+ysz], rowblur[I1->width*I1->height], colblur[I1->width*I1->height];
 	uchar *in, *out;
 
 	w = I1->width;		h = I1->height;		total = w * h;		in  = I1->image;
 
 	// init I2 dimensions and buffer
-	I2->width  = I1->width;
-	I2->height = I1->height;
+	I2->width  = I1->width;			I2->height = I1->height;
 	I2->image  = (uchar *) malloc(total);
 	if(I2->image == NULL) {
 		cerr << "blur: Insufficient memory\n";
 		exit(1);
 	}
-
-	xpad = (int)(xsz/2);		ypad = (int)(ysz/2);
-
-	out_itr=0;
-	for(y=0; y<h; y++) {
-		x=0;
-		mysum=0;
-		for(i=y*w; i<(y*w+w); i++) {
-			if(i%w==0 || i%w==(w-1)) {
-				for(pad_itr=0; pad_itr<xpad+1; pad_itr++) xline[x++] = in[i];
-			}
-			else xline[x++] = in[i];
+	out = I2->image;
+	// iterate from top to hottom
+	for(h_itr=0, out_itr=0, pad=(int)(xsz/2); h_itr<h; h_itr++) {
+		// iterate from left to right
+		for(i=h_itr*w, w_itr=0, mysum=0; i<(h_itr*w+w); i++) {
+			// if the row iter is on the first or last pixel of line,
+			// add xsz/2 pad pixels and the pixel itself to row buffer
+			if(i%w==0 || i%w==(w-1))
+				for(pad_itr=0; pad_itr<pad+1; pad_itr++) xline[w_itr++] = in[i];
+			// if the row itr is not on the first or last pixel of row, copy the pixel into the row buffer
+			else xline[w_itr++] = in[i];
 		}
-		
+		// sum of neighbor pixels for first pixel in image row
 		for(i=0; i<xsz; i++) mysum += xline[i];
-
-		for(x=xpad; x<xpad+w; x++){
-			rowblur[out_itr++] = (int)mysum/xsz;
-			mysum += (xline[x+xpad+1] - xline[x-xpad]);
+		// place average of row neighbors into temporary image
+		for(w_itr=pad; w_itr<pad+w; w_itr++){
+			rowblur[out_itr++] = mysum/xsz;
+			mysum += (xline[w_itr+pad+1] - xline[w_itr-pad]);
 		}
 	}
-
-	out_itr = 0;
-	for(y=0; y<w; y++) {
-		x=0;
-		mysum=0;
-		for(i=y; i<total; i+=w) {
-			if(i<w || i>=(total-w)) {
-				for(pad_itr=0; pad_itr<ypad+1; pad_itr++) yline[x++] = rowblur[i];
-			}
-			else yline[x++] = rowblur[i];
+	// iterate over the new image, with rows blurred, from top to bottom
+	for(h_itr=0, out_itr=0, pad=(int)(ysz/2); h_itr<w; h_itr++) {
+		// iterate over the new image, with rows blurred from left to right
+		for(i=h_itr, w_itr=0, mysum=0; i<total; i+=w) {
+			// if the row iter is on the first or last pixel of line,
+			// add ysz/2 pad pixels and the pixel itself to row buffer
+			if(i<w || i>=(total-w))
+				for(pad_itr=0; pad_itr<pad+1; pad_itr++) yline[w_itr++] = rowblur[i];
+			// if the row itr is not on the first or last pixel of row, copy the pixel into the row buffer
+			else yline[w_itr++] = rowblur[i];
 		}
-		
+		// sum of neighbor pixels for the first pixel in new image row
 		for(i=0; i<ysz; i++) mysum += yline[i];
-
-		for(x=ypad; x<ypad+h; x++){
-			colblur[out_itr++] = (int)mysum/ysz;
-			mysum += (yline[x+ypad+1] - yline[x-ypad]);
+		// place average of row neighbors into another temp image.
+		// the rows and columns trade places, making the image rotate 90 degrees
+		for(w_itr=pad; w_itr<pad+h; w_itr++){
+			colblur[out_itr++] = mysum/ysz;
+			mysum += (yline[w_itr+pad+1] - yline[w_itr-pad]);
 		}
 	}
-
-	out = I2->image;	// output image buffer
-	out_itr = 0;
-	for(y=0; y<w; y++) {
-		for(i=y; i<total; i+=w) {
-			out[out_itr++] = colblur[i];
-		}
-	}
+	// send pixels to output image after switching the rows and columns again to rotate back 90 degrees
+	for(h_itr=0, out_itr=0; h_itr<w; h_itr++)
+		for(i=h_itr; i<total; i+=w) out[out_itr++] = colblur[i];
 }
